300ef2db1e25bef970999764cdb37c939f1a9736
Guilherme Alves <iamalves.jpg@gmail.com>
2024-02-02 15:15:31 -0300

    feat: add login page

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,24 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { ThemeProvider } from './components/theme-provider.tsx'
+import { createBrowserRouter, RouterProvider} from 'react-router-dom'
+import Login from './pages/login/index.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider>
-      <App />
+      <RouterProvider router={router}/>
     </ThemeProvider>
   </React.StrictMode>,
 )
---
+++ src/pages/login/index.tsx
@@ -0,0 +1,68 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { Eye, EyeOff } from "lucide-react";
+
+export default function Login() {
+
+    const [userName, setUserName] = useState('')
+    const [password, setPassword] = useState('')
+    const [showPassword, setShowPassword] = useState(false)
+
+    const handleLogin = () => {
+        alert(userName + ' ' + password)
+    }
+
+    const handleShowPassword = () => {
+        setShowPassword(!showPassword)
+    }
+
+  return (
+    <div className="flex justify-center items-center h-screen">
+        <div className="flex flex-col items-center">
+            <span className="text-4xl font-bold">AWAKENING</span>
+            <span className="text-4xl font-bold text-blue-400 mb-12">RPG</span>
+            <div className="flex flex-col w-80">
+                <Input
+                    placeholder="Nome de usuário"
+                    className="mb-4 h-14"
+                    type="text"
+                    value={userName}
+                    onChange={(e) => setUserName(e.target.value)}
+                />
+                <div className="flex gap-2">
+                    <Input
+                        placeholder="Senha"
+                        className="mb-4 h-14 w-4/5"
+                        type={showPassword ? "text" : "password"}
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+
+                    />
+                    <Button
+                        size={"icon"}
+                        variant={"outline"}
+                        className="w-1/5 h-14"
+                        onClick={handleShowPassword}
+                    >
+                        {showPassword ? <EyeOff size={24} /> : <Eye size={24} />}
+                    </Button>
+                </div>
+                <div className="flex flex-col items-center">
+                    <Button
+                        size={"lg"}
+                        className="bg-blue-400 text-zinc-50 hover:bg-blue-300 mb-4"
+                        onClick={handleLogin}
+                    >
+                        Entrar
+                    </Button>
+                    <Link to="/register" className="hover:underline underline-offset-4">
+                        Novo por aqui? Registre-se
+                    </Link>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
